b3ae17a9bade6b1d6130d5f1457d5017abcc6d21
shirleyalvarado.2003@gmail.com <shirleyalvarado.2003@gmail.com>
2023-07-13 02:27:54 -0500

    Clase del 07 julio 2023

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+  slides = [
+    {
+      titulo: 'Gatitos',
+      img: 'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-1248x770@Las%20Provincias.jpg',
+      icon: 'paw-outline',
+      description: 'hola soy la explicacion de slide,hola soy la explicacion de slide',
+    },
+
+    {
+      titulo: 'Perritos',
+      img: 'https://st2.depositphotos.com/3378121/5193/i/600/depositphotos_51933801-stock-photo-labrador-puppies-in-a-basket.jpg',
+      icon: 'paw-outline',
+      description: 'hola soy la explicacion de slide,hola soy la explicacion de slide',
+    },
+
+    {
+      titulo: 'hamster',
+      img: 'https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRSM-bLdlw42S0tP6jHNppEhfDDU2nwKRL9UzKv7Mx6uOay9N4RsJLJmst9VIxAOckx',
+      icon: 'paw-outline',
+      description: 'hola soy la explicacion de slide,hola soy la explicacion de slide'
+    },
+    
+    {
+      titulo: 'Gatitos',
+      img: 'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-1248x770@Las%20Provincias.jpg',
+      icon: 'paw-outline',
+      description: 'hola soy la explicacion de slide,hola soy la explicacion de slide',
+    }
+  
+  ]
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+  final(){
+    this.router.navigateByUrl("/home");
+
+  }
+
+}
