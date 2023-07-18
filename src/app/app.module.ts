bbfda04dec5797fff2b35f0c3408ae276ca9ae69
shirleyalvarado.2003@gmail.com <shirleyalvarado.2003@gmail.com>
2023-07-17 23:25:01 -0500

    clase 10 de julio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-intro',
@@ -38,11 +39,13 @@ export class IntroPage implements OnInit {
   
   ]
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private storage:Storage) { }
 
   ngOnInit() {
   }
   final(){
+    console.log("Estoy tratando de finalizar")
+    this.storage.set("introShow", true)
     this.router.navigateByUrl("/home");
 
   }
