08f665f66b3c249fd97120b432362fd7aab58b99
shirleyalvarado.2003@gmail.com <shirleyalvarado.2003@gmail.com>
2023-07-24 00:26:51 -0500

    clase 17/07/2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MusicService } from '../services/music.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  artists:any;
+  localArtists:any;
+  constructor(private musicService: MusicService) {}
 
-  constructor() {}
+
+  ionViewDidEnter(){
+    this.musicService.getArtists().then(listArtists => {
+      this.artists = listArtists;
+      console.log("variable artists",this.artists);
+    })
+
+
+    this.localArtists = this.musicService.getArtistsFromJson();
+    console.log(this.localArtists.artists);
+  }
+
+  showSongs(artist: any){
+    console.log(artist);
+
+
+  }
 
 }
